449c56883adc35710652b223b1410dad17974ff0
it-support-tech <it-support@ntp-lao.com>
2026-08-11 15:05:19 +0700

    update code databse

--- config/Database.php
+++ config/Database.php
@@ -10,12 +10,27 @@ final class Database
 
     public static function connection(): PDO
     {
+        // if (self::$instance === null) {
+        //     $host = getenv('DB_HOST') ?: 'db';
+        //     $port = getenv('DB_PORT') ?: '5432';
+        //     $name = getenv('DB_NAME') ?: 'invoices';
+        //     $user = getenv('DB_USER') ?: 'invoice_app';
+        //     $pass = getenv('DB_PASS') ?: '';
+
+        //     $dsn = "pgsql:host={$host};port={$port};dbname={$name}";
+
+        //     self::$instance = new PDO($dsn, $user, $pass, [
+        //         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        //         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        //         PDO::ATTR_EMULATE_PREPARES => false,
+        //     ]);
+        // }
         if (self::$instance === null) {
-            $host = getenv('DB_HOST') ?: 'localhost';
-            $port = getenv('DB_PORT') ?: '5432';
-            $name = getenv('DB_NAME') ?: 'invoices';
-            $user = getenv('DB_USER') ?: 'root';
-            $pass = getenv('DB_PASS') ?: 'Psql@postgresNTP-2025';
+            $host = 'localhost';
+            $port = '5432';
+            $name = 'invoices';
+            $user = 'root';
+            $pass = 'Psql@postgresNTP-2025';
 
             $dsn = "pgsql:host={$host};port={$port};dbname={$name}";
 
